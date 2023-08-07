90f03aa9abf89eee8c17c5468bf0a73c85af40f3
Сергей Подлесных <oboltu@gmail.com>
2023-08-07 16:32:58 +0300

    add YtBackupLambda

---
+++ Functions/OnSheduledUpdateYouTrackBackups.py
@@ -0,0 +1,58 @@
+import json
+import boto3
+import os
+import urllib3
+import json
+import shutil
+
+def lambda_handler(event, context):
+    headers = {'Authorization': os.environ['YT_TOKEN'],
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'}
+                
+               
+    http = urllib3.PoolManager()
+
+    response = http.request("GET", os.environ['YT_URL'], headers=headers)
+
+    if response.status != 200:
+        print(response) #отправить письмо какое-то
+    
+    
+    class Backups:
+        items = []
+    
+        def __init__(self, backups_data):
+            for backup in backups_data:
+                self.items.append(Backup(backup['id']))
+    
+    
+    class Backup:
+        def __init__(self, backup_id):
+            self.id = backup_id
+            self.year, self.month, self.day, self.hours, self.minutes, self.seconds_and_extension = backup_id.split("-")
+            self.seconds, self.extension = self.seconds_and_extension.split(".", 1)
+    
+    jsonData = json.loads(response.data)
+    
+    backup = Backups(jsonData).items[0]
+
+    buketName = backup.id
+    
+    backup_fields = http.request("GET", os.environ['YT_BACKUPS_URL']+buketName+os.environ["FIELDS_ARGS"], headers=headers)
+    
+    backupLink = json.loads(backup_fields.data)['link']
+
+    s3 = boto3.client('s3')
+    
+    key = f"{backup.year}/{backup.month}/{backup.day}{backup.month}{backup.year}.{backup.extension}"
+
+    print(key)
+    
+    path = '/tmp/' + buketName
+    
+    with http.request('GET', os.environ['YT_API_URL']+backupLink, preload_content=False) as r, open(path, 'wb') as out_file:       
+        shutil.copyfileobj(r, out_file)
+
+    yt_bucket_obj = s3.upload_file(path, os.environ['YT_BUCKET'], key )
+    
